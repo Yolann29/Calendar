5fedd46a8a2c4947f946d2ac78c8c7b589de8925
Victor Morellet <victor.morellet@epitech.eu>
2023-12-06 20:02:52 +0100

    feat(Races): change some parts and have a double for distance

--- src/Days/Races.java
+++ src/Days/Races.java
@@ -5,17 +5,22 @@ import java.util.Arrays;
 
 public class Races {
 
-    public ArrayList<Integer> times, distances;
+//    public ArrayList<Integer> times, distances;
+    public long time;
+    public double distance;
 
     public Races() {
-        times = new ArrayList<>(Arrays.asList(62, 64, 91, 90));
-        distances = new ArrayList<>(Arrays.asList(553, 1010, 1473, 1074));
-        getResult();
+//        times = new ArrayList<>(Arrays.asList(62, 64, 91, 90));
+//        distances = new ArrayList<>(Arrays.asList(553, 1010, 1473, 1074));
+        time = 62649190;
+        distance = 553101014731074.;
+        System.out.println(getRace(time, distance));
+//        getResult();
     }
 
-    public int getRace(int timeMax, int distanceMax) {
-        int distance = 0;
-        int sum = 0;
+    public long getRace(long timeMax, double distanceMax) {
+        long distance = 0;
+        long sum = 0;
         for (int buttonPressedTime = 0; buttonPressedTime <= timeMax; buttonPressedTime++) {
             distance = (timeMax - buttonPressedTime) * buttonPressedTime;
             if (distance > distanceMax) sum += 1;
@@ -23,11 +28,11 @@ public class Races {
         return sum;
     }
 
-    public void getResult() {
-        int result = 1;
-        for (int i = 0; i < times.size(); i++) {
-            result *= getRace(times.get(i), distances.get(i));
-        }
-        System.out.println(result);
-    }
+//    public void getResult() {
+//        int result = 1;
+//        for (int i = 0; i < times.size(); i++) {
+//            result *= getRace(times.get(i), distances.get(i));
+//        }
+//        System.out.println(result);
+//    }
 }
