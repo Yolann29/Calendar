28322c039584f0959ef622a991c1c137a932d6d7
Victor Morellet <victor.morellet@epitech.eu>
2023-12-13 11:10:19 +0100

    feat(oasis): add part2 resolution

--- src/Oasis.cpp
+++ src/Oasis.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 int getChain(const string&, int);
+int getChain2(const string&, vector<int>);
 
 int main() {
     string file("Doc/Oasis");
@@ -24,8 +25,10 @@ int main() {
     }
 
     int sum = 0;
+    vector<int> res;
     for (string line : lines) {
-        sum += getChain(line, 0);
+        // sum += getChain(line, 0);
+        sum += getChain2(line, res);
     }
 
     cout << sum << endl;
@@ -46,5 +49,27 @@ int getChain(const string& line, int res) {
         newLine += to_string(stoi(chain.at(i + 1)) - stoi(chain.at(i))) + " ";
     }
     if (zero == false) return getChain(newLine, res);
+    return res;
+}
+
+int getChain2(const string& line, vector<int> add) {
+    vector<string> chain;
+    stringstream stringstream(line);  
+    string word;
+    while (stringstream >> word) {
+        chain.push_back(word);
+    }
+    add.push_back(stoi(chain.at(0)));
+    string newLine;
+    bool zero = true;
+    for (int i = 0; i < chain.size() - 1; i++) {
+        if (stoi(chain.at(i)) != 0) zero = false;
+        newLine += to_string(stoi(chain.at(i + 1)) - stoi(chain.at(i))) + " ";
+    }
+    if (zero == false) return getChain2(newLine, add);
+    int res = 0;
+    for (int i = add.size() - 1; i >= 0; i--) {
+        res = add.at(i) - res;
+    }
     return res;
 }
